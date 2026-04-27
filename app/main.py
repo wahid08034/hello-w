584ded0e36dfe402303b236a56e44593e35c2c33
wahaduzzaman <38437616+wahid08034@users.noreply.github.com>
2026-04-27 12:26:42 +0600

    Update welcome message in read_root endpoint
    2nd commit

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World from Python in ECS!"}
+    return {"message": "Hello World from Python in ECS! & welcome to vercel arena"}
 
 @app.get("/health")
 def health_check():
